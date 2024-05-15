8d25d098b38267c048f94e1718aaa9b2bbe34222
ZJH111 <2039065994@qq.com>
2024-05-15 20:30:50 +0800

    2024年5月15日20:30:36

--- src/test.java
+++ src/test.java
@@ -18,7 +18,7 @@ import java.util.concurrent.TimeUnit;
 
 public class test {
 
-    public static String path ="D:\\JAVAPROJECT\\rfidSport\\newData\\positionA\\侧平举\\zjs";
+    public static String path ="D:\\JAVAPROJECT\\rfidSport\\newData\\positionC\\推肩\\zjs";
 
     //采集间隔 period
     public static int period = 6;
